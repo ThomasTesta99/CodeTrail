234e315371d32d23daf9868e3afa772027c75333
Thomas Testa <ttesta99@yahoo.com>
2025-06-24 15:23:02 -0400

    remove comments

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -9,17 +9,14 @@ const layout = async ({children}: {children: ReactNode}) => {
   const user = session?.user;
   return (
     <main className="flex flex-col md:flex-row w-full min-h-screen overflow-x-hidden">
-      {/* Topbar: visible only on small screens */}
       <div className="md:hidden">
         <Topbar user={user} />
       </div>
 
-      {/* Sidebar: visible only on md+ screens */}
       <div className="hidden md:block">
         <Sidebar user={user} />
       </div>
 
-      {/* Main content */}
       <div className="flex-1 flex flex-col min-w-0">
         {children}
       </div>

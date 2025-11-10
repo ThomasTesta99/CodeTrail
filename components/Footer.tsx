d6b864b4482eceed97fe673d1b8655230bcd691c
Thomas Testa <ttesta99@yahoo.com>
2025-11-10 12:30:47 -0500

    Attempt#3 at Fixing Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,59 +5,43 @@ import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 import React from 'react'
 
-const Footer = ({ user }: UserProps) => {
-  const router = useRouter();
+const Footer = ({user}: UserProps) => {
+    const router = useRouter();
 
-  const signOut = () => {
-    logoutUser();
-    router.push('/sign-in');
-  };
+    const signOut = () => {
+        logoutUser();
+        router.push('/sign-in')
+    }
 
-  const hasImage = !!user?.image;
+    const hasImage = !!user?.image
+    
+    return (
+        <footer className="footer mt-auto">
+            <div className= {hasImage ? "footer-name-with-image bg-transparent" : "footer-name"}>
+                {user?.image ? (
+                    <Image src={user.image} alt="User image" width={100} height={100} className="rounded-full" />
+                ) : (
+                    <p className="text-xl font-bold text-white">
+                        {user?.name?.[0] ?? ' '}
+                    </p>
+                )}
+            </div>
 
-  return (
-    <footer className="footer mt-auto">
-      <div
-        className= {
-          hasImage ? 'footer-name-with-image' : 'footer-name'
-        }
-      >
-        {hasImage ? (
-          <Image
-            src={user.image!}
-            alt="User image"
-            fill
-            className="rounded-full object-cover"
-          />
-        ) : (
-          <p className="text-xl font-bold text-white">
-            {user?.name?.[0] ?? ' '}
-          </p>
-        )}
-      </div>
+            <div className="footer-email">
+                <h1 className="text-sm truncate font-semibold text-white">
+                    {user?.name ?? ' '}
+                </h1>
+                <p className="text-sm truncate font-normal text-gray-300">
+                     {user?.email ?? ' '}
+                </p>
+            </div>
 
-      <div className="footer-email">
-        <h1 className="text-sm truncate font-semibold text-white">
-          {user?.name ?? ' '}
-        </h1>
-        <p className="text-sm truncate font-normal text-gray-300">
-          {user?.email ?? ' '}
-        </p>
-      </div>
+            <button onClick={signOut} className={user?.image ? "footer-image-large" : "footer-image"}>
+                <Image src="/assets/icons/logout.svg" fill alt="logout" className="brightness-0 invert" />
+            </button>
+        </footer>
 
-      <button
-        onClick={signOut}
-        className={hasImage ? 'footer-image-large' : 'footer-image'}
-      >
-        <Image
-          src="/assets/icons/logout.svg"
-          alt="logout"
-          fill
-          className="brightness-0 invert"
-        />
-      </button>
-    </footer>
-  );
-};
+    )
+}
 
-export default Footer;
+export default Footer

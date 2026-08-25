67d07d3b6814d39a1cadff9f2340238dd89f97b5
Thomas Testa <ttesta99@yahoo.com>
2026-08-25 14:42:22 -0400

    fix email not sending

--- lib/email.ts
+++ lib/email.ts
@@ -6,7 +6,7 @@ export const sendEmail = async ({
   resetLink: string;
 }) => {
   const serviceID = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID!;
-  const templateID = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID!;
+  const templateID = process.env.EMAILJS_TEMPLATE_ID!;
   const publicKey = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY!;
   const privateKey = process.env.EMAILJS_PRIVATE_KEY!;
 
